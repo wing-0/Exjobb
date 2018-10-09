488dc88c95ca0c9373cd88c825e1bee010bea97d
Niklas Wingren <38071649+wing-0@users.noreply.github.com>
2018-10-09 17:32:30 +0200

    Add eps to gitignore. Add another plot to the phi plotter (maximum geometry). Write more on the seminar presentation and add some figures (not included here). Some minor writing in the review document.

--- Kod/density-variation-rre-phifunction.py
+++ Kod/density-variation-rre-phifunction.py
@@ -10,9 +10,12 @@ import matplotlib.pyplot as plt
 
 plt.close('all')
 lam = 5e-3
-Lam = 2.5e-3
+Lam = 2.89e-3
 k = 2*np.pi/lam
 q = 2*np.pi/Lam
+
+# Machine epsilon used to avoid "divide by zero" errors in max tracing
+eps = np.finfo(float).eps
 
 # Phi angular function
 def pphi(pm,theta,phi,alpha):
@@ -37,7 +40,7 @@ p = pphi(1,theta,phi_m,alpha_m)**2
 m = pphi(-1,theta,phi_m,alpha_m)**2
 
 plt.figure()
-plt.pcolormesh(np.degrees(alpha_m),np.degrees(phi_m),p)
+plt.pcolormesh(np.degrees(alpha_m),np.degrees(phi_m),p,cmap='plasma')
 plt.xlabel(r'$\alpha$ [$^\circ$]')
 plt.ylabel(r'$\varphi$ [$^\circ$]')
 plt.colorbar(label=r'$\Phi^{+^2}$')
@@ -48,15 +51,21 @@ plt.suptitle('$\\Phi$ angular function $\\varphi$ and $\\alpha$ dependence ' + \
 plt.title('EM prop.dir. at $\\varphi = 180^\\circ$, acoustic prop.dir. at ' + \
           '$\\varphi = 180^\\circ + \\alpha$',fontsize=10)
 
+# Plot point of maximum
+phim = 2*np.arctan(np.sqrt(lam**2/(4*Lam**2-lam**2+eps)))
+alpham = np.arccos(-lam/2/Lam)
+plt.scatter(np.degrees(alpham),np.degrees(phim),marker='.')
+plt.xlim([0,180])
+
 #plt.figure()
 #plt.pcolormesh(np.degrees(alpha_m),np.degrees(phi_m),\
-#               np.abs(np.cos(alpha_m)+q/2/k) + np.abs(np.tan(phi_m/2)-np.sqrt(q**2/(4*k**2-q**2))) ,\
+#               np.abs(np.cos(alpha_m)-q/2/k) + np.abs(np.tan(phi_m/2)+np.sqrt(q**2/(4*k**2-q**2))) ,\
 #               vmin=-0.2,vmax=0.2,cmap='seismic')
 #plt.yticks(np.linspace(0,360,10))
 #plt.colorbar()
 
 plt.figure()
-plt.pcolormesh(np.degrees(alpha_m),np.degrees(phi_m),m)
+plt.pcolormesh(np.degrees(alpha_m),np.degrees(phi_m),m,cmap='plasma')
 plt.xlabel(r'$\alpha$ [$^\circ$]')
 plt.ylabel(r'$\varphi$ [$^\circ$]')
 plt.colorbar(label=r'$\Phi^{-^2}$')
@@ -67,7 +76,88 @@ plt.suptitle('$\\Phi$ angular function $\\varphi$ and $\\alpha$ dependence ' + \
 plt.title('EM prop.dir. at $\\varphi = 180^\\circ$, acoustic prop.dir. at ' + \
           '$\\varphi = 180^\\circ + \\alpha$',fontsize=10)
 
-stop
+# Plot point of maximum, +2pi since negative arctan gives negative angle
+phim = 2*np.arctan(-np.sqrt(lam**2/(4*Lam**2-lam**2+eps))) + 2*np.pi
+alpham = np.arccos(lam/2/Lam)
+plt.scatter(np.degrees(alpham),np.degrees(phim),marker='.')
+plt.xlim([0,180])
+
+# %% Variation of max with changing wavelength fraction - polar plot (+)
+plt.close('all')
+plt.figure()
+
+# Fraction EM lambda/AO lambda
+lamfr = np.array([0.1,1,1.99])
+alpha = np.arccos(-lamfr/2)
+phi = 2*np.arctan(np.sqrt(lamfr**2/4/(1-lamfr**2/4+eps)))
+
+# Incident EM
+plt.polar(np.pi*np.ones(2),np.arange(2),'k',label='$\mathbf{\hat{k}}$')
+# Arrow for incident EM
+plt.polar((np.pi-0.2)*np.ones(2),np.array([0,0.1]),'k',\
+          (np.pi+0.2)*np.ones(2),np.array([0,0.1]),'k')
+
+# Colors
+c = ['r','g','b']
+
+for i in range(0,len(lamfr)):
+    plt.polar((np.pi+alpha[i])*np.ones(2),np.arange(2),c[i],linestyle=':',\
+              label='$\mathbf{\hat{q}}, \lambda/\Lambda = $'+str(lamfr[i]))
+    plt.polar(alpha[i]*np.ones(2),np.arange(2),c[i],linestyle=':')
+    # Arrow for sound
+    plt.polar(np.array([alpha[i]+0.03,alpha[i]]),np.array([0.9,1]),c[i],\
+          np.array([alpha[i]-0.03,alpha[i]]),np.array([0.9,1]),c[i])
+    
+    plt.polar(phi[i]*np.ones(2),np.arange(2),c[i],\
+              label='$\mathbf{\hat{k}}_{sc}, \lambda/\Lambda = $'+str(lamfr[i]))
+    # Arrow for scattered EM
+    plt.polar(np.array([phi[i]+0.03,phi[i]]),np.array([0.9,1]),c[i],\
+          np.array([phi[i]-0.03,phi[i]]),np.array([0.9,1]),c[i])
+
+plt.title('Optimal scattering geometry for different $\lambda/\Lambda$ (+ case)')
+plt.ylim(0,1)
+plt.yticks(np.linspace(0,1,5),[])
+
+plt.figlegend(loc=4,framealpha=1)
+
+# %% Variation of max with changing wavelength fraction - polar plot (-)
+plt.close('all')
+plt.figure()
+
+# Fraction EM lambda/AO lambda
+lamfr = np.array([0.1,1,1.99])
+alpha = np.arccos(lamfr/2)
+phi = 2*np.arctan(-np.sqrt(lamfr**2/4/(1-lamfr**2/4+eps))) + 2*np.pi
+
+# Incident EM
+plt.polar(np.pi*np.ones(2),np.arange(2),'k',label='$\mathbf{\hat{k}}$')
+# Arrow for incident EM
+plt.polar((np.pi-0.2)*np.ones(2),np.array([0,0.1]),'k',\
+          (np.pi+0.2)*np.ones(2),np.array([0,0.1]),'k')
+
+# Colors
+c = ['r','g','b']
+
+for i in range(0,len(lamfr)):
+    plt.polar((np.pi+alpha[i])*np.ones(2),np.arange(2),c[i],linestyle=':',\
+              label='$\mathbf{\hat{q}},$ $\lambda/\Lambda = $'+str(lamfr[i]))
+    plt.polar(alpha[i]*np.ones(2),np.arange(2),c[i],linestyle=':')
+    # Arrow for sound
+    plt.polar(np.array([alpha[i]+0.03,alpha[i]]),np.array([0.9,1]),c[i],\
+          np.array([alpha[i]-0.03,alpha[i]]),np.array([0.9,1]),c[i])
+    
+    plt.polar(phi[i]*np.ones(2),np.arange(2),c[i],\
+              label='$\mathbf{\hat{k}}_{sc},$ $\lambda/\Lambda = $'+str(lamfr[i]))
+    # Arrow for scattered EM
+    plt.polar(np.array([phi[i]+0.03,phi[i]]),np.array([0.9,1]),c[i],\
+          np.array([phi[i]-0.03,phi[i]]),np.array([0.9,1]),c[i])
+
+plt.title('Optimal scattering geometry for different $\lambda/\Lambda$ (- case)')
+plt.ylim(0,1)
+plt.yticks(np.linspace(0,1,5),[])
+
+plt.figlegend(loc=4,framealpha=1)
+
 # %% Some angles
 plt.figure()
 alpha = np.radians(np.array([15,30,60,180]))

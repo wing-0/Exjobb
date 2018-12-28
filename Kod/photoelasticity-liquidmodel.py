Fix a sign error which affected the figure for the photoelastic model. Also some major improvements in the acoustic and photoelastic theory parts in the report

Kod/photoelasticity-liquidmodel.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 er = np.linspace(1,10)
-p = -1/3*(er-1)*(er+2)/er**2
+p = 1/3*(er-1)*(er+2)/er**2
 
 plt.figure(figsize=(5,4))
 
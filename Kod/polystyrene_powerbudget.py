cba11a3e5efa5b59c11585b54a6982e719585a95
Niklas Wingren <38071649+wing-0@users.noreply.github.com>
2019-01-23 22:08:38 +0100

    Finish report, add some scripts which were not used in the report. Add references

--- Kod/polystyrene_powerbudget.py
+++ Kod/polystyrene_powerbudget.py
@@ -15,6 +15,9 @@ rho_0 = 1070                # Density
 v = 2400                    # Speed of sound
 er = 2.6                    # Relative permittivity
 
+# Bulk modulus (fluid)
+K = rho_0*v**2
+
 # Photoelastic constant from scalar model
 ph = 1/3*(er-1)*(er+2)/er**2
 
@@ -32,8 +35,8 @@ br = (apa + 2*d_ref*np.tan(angle_a/2))/2
 
 I_s = P_s/(np.pi*br**2)     # Sound intensity at interection
 
-# Sound strain amplitude at intersection
-s_p = np.sqrt(2*I_s/rho_0/v**3)
+# Sound pressure amplitude at intersection
+p0 = np.sqrt(2*I_s*rho_0*v)
 
 # EM antenna properties
 fe = 60e9                   # Frequency
@@ -49,22 +52,18 @@ ape = np.sqrt(G*le**2/4/np.pi)
 alpha = np.arccos(le/2/la)
 
 # Range for EM transmitter and receiver - based on EM tx, Ac. tx, EM rx placed
-# on a straight line, so only reasonable for small alpha
-#R = d_ref/np.cos(alpha)
-
-# Range for EM transmitter and receiver - based on EM tx, Ac. tx, EM rx placed
 # on the edge of a circle with center in the intersection point
 R = d_ref
 
 # Approximate beam widths using both transducer/antenna aperture size and
 # beam spread from aperture to intersection
-wa = apa + 2*d_ref*np.tan(angle_a/2)
-we = ape + 2*R*np.tan(angle_e/2)
-Lz = min([we, wa])
+da = apa + 2*d_ref*np.tan(angle_a/2)
+de = ape + 2*R*np.tan(angle_e/2)
+Lz = min([de, da])
 
 # Overlap length based on parallelogram model with cosine law for diagonal
-d = np.sqrt((we/np.sin(np.pi-alpha))**2 + (wa/np.sin(np.pi-alpha))**2 -
-            2*we*wa/np.sin(np.pi-alpha)**2 * np.cos(np.pi-alpha))
+d = np.sqrt((de/np.sin(np.pi-alpha))**2 + (da/np.sin(np.pi-alpha))**2 -
+            2*de*da/np.sin(np.pi-alpha)**2 * np.cos(np.pi-alpha))
 
 # Receiver things
 T0 = 290                        # Standard temperature, 290 K
@@ -72,8 +71,8 @@ NF = 5                          # Noise figure in dB
 F = 10**(NF/10)                 # Noise factor, linear
 B = fa                          # Assume receiver BW same as Ac. freq.
 
-# RCS, assuming optimal receiver placement (aka Phi_p = 1/sin(alpha))
-sigma = er**2*k**2/16/np.pi*ph**2*s_p**2*wa**2*we**2*Lz**2/np.sin(alpha)
+# RCS, assuming optimal receiver placement
+sigma = er**2*k**4*ph**2*p0**2/16/np.pi/K**2 * da**2*de**2*Lz**2/np.sin(alpha)
 
 # Power required to obtain 0 dB SNR with 1 sample
 P = (4*np.pi)**3 * R**4 * con.k*T0*B*F/(G**2 * le**2 * sigma)
